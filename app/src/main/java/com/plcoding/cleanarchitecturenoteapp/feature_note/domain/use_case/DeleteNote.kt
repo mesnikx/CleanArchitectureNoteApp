3bf464d1374b52c1eeabf450bbe820a5b9926224
Michael Rudakov <mishelrudakov@yandex.ru>
2023-12-02 14:04:54 +0300

    ViewModel setup

--- app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/domain/use_case/DeleteNote.kt
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/domain/use_case/DeleteNote.kt
@@ -1,12 +1,12 @@
 package com.plcoding.cleanarchitecturenoteapp.feature_note.domain.use_case
 
-import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.model.Note
+import android.provider.ContactsContract
 import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.repository.NoteRepository
 
 class DeleteNote(
     private val repository: NoteRepository
 ) {
-    suspend operator fun invoke(note: Note) {
+    suspend operator fun invoke(note: ContactsContract.CommonDataKinds.Note) {
         repository.deleteNote(note)
     }
 }
---
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/presentation/notes/NotesEvent.kt
@@ -0,0 +1,11 @@
+package com.plcoding.cleanarchitecturenoteapp.feature_note.presentation.notes
+
+import android.provider.ContactsContract.CommonDataKinds.Note
+import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.util.NoteOrder
+
+sealed class NotesEvent {
+    data class Order(val noteOrder: NoteOrder): NotesEvent()
+    data class DeleteNote(val note: Note): NotesEvent()
+    object RestoreNote: NotesEvent()
+    object ToggleOrderSection: NotesEvent()
+}

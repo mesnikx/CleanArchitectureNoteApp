584f081373639bdb2f4e28f39135e4e44701b478
Michael Rudakov <mishelrudakov@yandex.ru>
2023-12-03 19:56:18 +0300

    Default Radio Button Compose

---
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/presentation/notes/components/DefaultRadioButton.kt
@@ -0,0 +1,15 @@
+package com.plcoding.cleanarchitecturenoteapp.feature_note.presentation.notes.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun DefaultRadioButton(
+    text: String,
+    checked: Boolean,
+    onCheck: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+
+
+}
